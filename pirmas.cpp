91088947b262329374a4b98d01c42d30aabb0735
robke0224 <154459735+robke0224@users.noreply.github.com>
2024-02-09 13:02:38 +0200

    Update pirmas.cpp

--- pirmas.cpp
+++ pirmas.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
 #include <string>
+
+using namespace std;
+ struct stud {
+string vard,pav;
+int rez;
+};
+
+int main() {
+  int n;
+n=0;
+stud *grupe = new stud[n];
+cout << "kiek studentu yra grupeje?";
+cin >> n;
+delete [] grupe;
+
+grupe =  new stud [n];
+
+for (int i=0; i,n; i++){
+cin >> 
