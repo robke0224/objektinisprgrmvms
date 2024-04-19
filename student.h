d8a74ffec9db9b68e38e5328264aed1564a09df9
robke0224 <154459735+robke0224@users.noreply.github.com>
2024-04-19 05:31:45 +0300

    Update student.h

--- student.h
+++ student.h
@@ -10,6 +10,8 @@
 #include <algorithm>
 #include <numeric>
 #include <random>
+#include <ctime>
+
 
 using namespace std;
 
@@ -20,18 +22,33 @@ struct Student {
     double median;
     vector<int> grades;
     int exam_results;
+    vector<int> pazymiai;
+    double egzaminoRezultatas;
+    double gal_vid;
+    double gal_med;
+
 };
 
-void generateRandomGrades(vector<Student>& students, double hw);
-void generateRandomData(vector<Student>& students, double hw);
-double calculateMedian(vector<int>& arr);
-void readDataFromFile(vector<Student>& students, double& hw, int N);
-void enterDataManually(vector<Student>& students, double hw);
+void generateRandomGrades (vector<Student>& students, double hw);
+void generateRandomData (vector<Student>& students, double hw);
+double calculateMedian (vector<int>& arr);
+void readDataFromFile (vector<Student>& students, double& hw, int N);
+void enterDataManually (vector<Student>& students, double hw);
 bool compareByName(const Student& a, const Student& b);
 bool compareBySurname(const Student& a, const Student& b);
 bool compareByMedian(const Student& a, const Student& b);
 bool compareByAvg(const Student& a, const Student& b);
 int generateRandomNumber(int min, int max);
-double calculateAverage(int arr[], int size);
+double calculateAverage(const vector<int>& pazymiai);
+void failuGeneravimas(int studentu_kiekis, const std::string& failo_pavadinimas);
+bool Nuskaitymas(const std::string& failo_pavadinimas, std::vector<Student>& students, int studentukiekis);
+std::chrono::steady_clock::time_point DabartinisLaikas();
+double LaikoSkirtumas(const std::chrono::steady_clock::time_point& pradzia, const std::chrono::steady_clock::time_point& pabaiga);
+void calculateResults(std::vector<Student>& stud);
+void rūšiuoja_ir_rašo_failus(const std::vector<Student>& students);
+double skaiciuotiVidurki(const std::vector<int>& pazymiai);
+bool arGerasStudentas(const Student& student);
+bool lygintiPagalVidurki(const Student& a, const Student& b);
+
 
 #endif 
