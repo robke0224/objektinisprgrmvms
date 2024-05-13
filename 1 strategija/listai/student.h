b5dcd0aa123f55dc44075635779659a39272c274
robke0224 <154459735+robke0224@users.noreply.github.com>
2024-05-13 22:15:02 +0300

    Create student.h

---
+++ 1 strategija/listai/student.h
@@ -0,0 +1,55 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <limits>
+#include <string>
+#include <list>
+#include <sstream>
+#include <algorithm>
+#include <numeric>
+#include <random>
+#include <ctime>
+#include <chrono>
+
+
+ using namespace std;
+
+struct Student {
+    string name, vardas;
+    string sur, pavarde;
+    double final_avg;
+    double median;
+    list<int> grades;
+    double exam_results;
+    list<int> pazymiai;
+    double egzaminoRezultatas;
+    double gal_vid;
+    double gal_med;
+
+};
+
+void generateRandomGrades(list<Student>& students, double hw);
+void generateRandomData(list<Student>& students, double hw);
+double calculateMedian(list<int>& arr);
+void readDataFromFile(list<Student>& students, double& hw, int N);
+void enterDataManually(list<Student>& students, double hw);
+bool compareByName(const Student& a, const Student& b);
+bool compareBySurname(const Student& a, const Student& b);
+bool compareByMedian(const Student& a, const Student& b);
+bool compareByAvg(const Student& a, const Student& b);
+int generateRandomNumber(int min, int max);
+double calculateAverage(const list<int>& pazymiai);
+void failuGeneravimas(int studentu_kiekis, const string& failo_pavadinimas);
+bool Nuskaitymas(const string& failo_pavadinimas, list<Student>& students, int studentukiekis);
+chrono::steady_clock::time_point DabartinisLaikas();
+double LaikoSkirtumas(const chrono::steady_clock::time_point& pradzia, const chrono::steady_clock::time_point& pabaiga);
+void calculateResults(list<Student>& stud);
+void rūšiuoja_ir_rašo_failus(const list<Student>& students);
+double skaiciuotiVidurki(const list<int>& pazymiai);
+bool arGerasStudentas(const Student& student);
+bool lygintiPagalVidurki(const Student& a, const Student& b);
+
+#endif // STUDENT_H
